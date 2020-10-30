a6f192745c4297b65db3c1738f2ee4f8577f1d50
JandryJaramillo <jandry_josue16@hotmail.com>
2020-10-30 10:48:41 -0500

    programas clase 3

--- Programas_en_clase/Saludo2.java
+++ Programas_en_clase/Saludo2.java
@@ -2,7 +2,7 @@ public class Saludo2 {
 	public static void main(String[] args) {
 		String nombre;
 		System.out.print("Por favor, dime como te llamas: ");
-		nombre = System.console().readline();
+		nombre = System.console().readLine();
 		System.out.print("Hola " + nombre);
 		System.out.println(", ¡encantado de conocerte!");
 	}
